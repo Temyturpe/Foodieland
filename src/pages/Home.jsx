2a4e1581c740a1536fc9cff4fa6534fc26534ddf
Temitope <themyturpeokesanya@gmail.com>
2022-12-07 16:10:52 +0100

    category page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HeaderMain from "../components/Header/HeaderMain";
-;
+import Categories from "../components/Categories/Categories";
 
 const Home = () => {
   return (
     <div className="w-full overflow-hidden">
       <HeaderMain />
-      {/* <Categories />
+      <Categories />
+      {/* 
       <Homerecipes home={Homerecipedata} />
       <Everyone />
       <Instagram />
